1d0ebd1f5316f3807f5ca2d563cbb89af510814f
Alvaro Romero <Alvaro.Romero.Ayala@everis.com>
2019-11-12 13:49:28 +0100

    first commit

---
+++ src/test/java/com/heroku/herokudemo/HerokudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.herokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokudemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
